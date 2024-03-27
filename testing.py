852280cec49d8bcaf67bc626fd042dcc079b302b
Ricardo Saraiva <159549682+MrRSaraiva@users.noreply.github.com>
2024-03-27 16:49:22 +0000

    Update testing.py
    Solved Issues #2 and #3

--- testing.py
+++ testing.py
@@ -1,2 +1,7 @@
+# This is a test (Solving Issue #2)
+
+# Solving Issue #3
+print("This program is a test. It is not meant to be functional. You can simply type something and it will be displayed.")
+
 user_input = input()
 print(user_input)
